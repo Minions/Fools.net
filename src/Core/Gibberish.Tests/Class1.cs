031792ac4dced5b485ed5ab3938c836899e15e38
Jay Bazuzi <JAY@BAZUZI.COM>
2015-12-20 13:53:46 -0800

    Initial commit, with trivial unit test

---
+++ src/Core/Gibberish.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Gibberish.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyTestMethod()
+        {
+        }
+    }
+}
---
+++ src/Core/Gibberish/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gibberish
+{
+    public class Class1
+    {
+        // need to be able to compile one file in a legit. way
+        // check with a particular name exists
+        // and is a thunk
+
+    }
+}
