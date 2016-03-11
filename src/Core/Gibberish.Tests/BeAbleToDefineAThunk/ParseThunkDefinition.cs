4a8c4df6718a8730af27160e216e1d7e488b42ea
Arlo Belshee <arlo.belshee@live.com>
2016-03-11 07:56:12 -0800

    Moved spec for use language to the right place.

--- src/Core/Gibberish.Tests/BeAbleToDefineAThunk/ParseThunkDefinition.cs
+++ src/Core/Gibberish.Tests/BeAbleToDefineAThunk/ParseThunkDefinition.cs
@@ -17,14 +17,5 @@ namespace Gibberish.Tests.BeAbleToDefineAThunk
 			var result = subject.GetMatch(input, subject.TopLevelStatement);
 			Approvals.VerifyJson(result.PrettyPrint());
 		}
-
-		[Test]
-		public void just_a_uselanguage_statement_should_be_empty_parse()
-		{
-			var input = "use language fasm\r\n";
-			var subject = new ParseFasm();
-			var result = subject.GetMatch(input, subject.FasmFile);
-			Approvals.VerifyJson(result.PrettyPrint());
-		}
 	}
 }
---
+++ src/Core/Gibberish.Tests/PickTheRightLanguageForFile/ParserChooseLanguageBasedOnUseLanguageStatement.cs
@@ -0,0 +1,19 @@
+﻿using ApprovalTests;
+using Gibberish.Tests.ZzTestHelpers;
+using NUnit.Framework;
+
+namespace Gibberish.Tests.PickTheRightLanguageForFile
+{
+	[TestFixture]
+	public class ParserChooseLanguageBasedOnUseLanguageStatement
+	{
+		[Test]
+		public void just_a_uselanguage_statement_should_be_empty_parse()
+		{
+			var input = "use language fasm\r\n";
+			var subject = new ParseFasm();
+			var result = subject.GetMatch(input, subject.FasmFile);
+			Approvals.VerifyJson(result.PrettyPrint());
+		}
+	}
+}
