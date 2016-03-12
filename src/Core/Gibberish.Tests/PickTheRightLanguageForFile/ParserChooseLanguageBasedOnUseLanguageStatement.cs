b753d0b963d91c55c4221f78456c3ab2da903ab7
Arlo Belshee <arlo.belshee@live.com>
2016-03-12 07:15:04 -0800

    Add ignored tests for error conditions.

--- src/Core/Gibberish.Tests/PickTheRightLanguageForFile/ParserChooseLanguageBasedOnUseLanguageStatement.cs
+++ src/Core/Gibberish.Tests/PickTheRightLanguageForFile/ParserChooseLanguageBasedOnUseLanguageStatement.cs
@@ -1,5 +1,4 @@
 ﻿using ApprovalTests;
-using ApprovalTests.Reporters;
 using Gibberish.Tests.ZzTestHelpers;
 using NUnit.Framework;
 
@@ -18,7 +17,8 @@ namespace Gibberish.Tests.PickTheRightLanguageForFile
 		}
 
 		[Test]
-		public void use_language_fasm_should_be_able_to_get_empty_thunk_def_in_that_language() {
+		public void use_language_fasm_should_be_able_to_get_empty_thunk_def_in_that_language()
+		{
 			var input = @"use language fasm
 
 define.named.thunk some.name:
@@ -28,5 +28,11 @@ define.named.thunk some.name:
 			var result = subject.GetMatch(input, subject.File);
 			Approvals.VerifyJson(result.PrettyPrint());
 		}
+
+		[Test, Ignore("Not implemented; should be in lang spec.")]
+		public void unknown_lang_should_give_nice_error() { }
+
+		[Test, Ignore("Not implemented; should be in lang spec.")]
+		public void missing_use_language_should_give_nice_error() { }
 	}
 }
