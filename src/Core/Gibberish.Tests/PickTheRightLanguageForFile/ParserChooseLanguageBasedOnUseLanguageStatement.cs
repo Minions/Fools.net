29f0f89a653c9b83150ab45ae7bdcf94dcd0911e
Arlo Belshee <arlo.belshee@live.com>
2016-03-13 13:05:13 -0700

    Make one test more explicit, using shoulds rather than approvals. Faster and all the info is inside the test (spec is in one place).

--- src/Core/Gibberish.Tests/PickTheRightLanguageForFile/ParserChooseLanguageBasedOnUseLanguageStatement.cs
+++ src/Core/Gibberish.Tests/PickTheRightLanguageForFile/ParserChooseLanguageBasedOnUseLanguageStatement.cs
@@ -1,4 +1,5 @@
 ﻿using ApprovalTests;
+using Gibberish.AST;
 using Gibberish.Parsing;
 using Gibberish.Tests.ZzTestHelpers;
 using NUnit.Framework;
@@ -30,17 +31,17 @@ define.named.thunk some.name:
 			Approvals.VerifyJson(result.PrettyPrint());
 		}
 
-		[Test, Timeout(1000)]
+		[Test, Timeout(100)]
 		public void use_language_fools_should_be_able_to_get_empty_namespace_in_that_language()
 		{
 			var input = @"use language fools
 
-in.namespace some.name:
+in.namespace the.name:
 	pass
 ";
 			var subject = new ParseLanguageFile();
 			var result = subject.GetMatch(input, subject.File);
-			Approvals.VerifyJson(result.PrettyPrint());
+			result.Should().Declare(FoolsAst.Namespace("the.name"));
 		}
 
 		[Test, Timeout(1000)]
